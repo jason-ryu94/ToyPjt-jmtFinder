19b0db2842c9bce802972c3083d7da51620c0e4d
jason-ryu94 <wptmd0918@hanmail.net>
2021-11-06 01:56:33 +0900

    토이프로젝트 생성

---
+++ src/main/java/jason/toyproject/jmtfinder/JmtFinderApplication.java
@@ -0,0 +1,13 @@
+package jason.toyproject.jmtfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmtFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmtFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jason/toyproject/jmtfinder/JmtFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package jason.toyproject.jmtfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmtFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
