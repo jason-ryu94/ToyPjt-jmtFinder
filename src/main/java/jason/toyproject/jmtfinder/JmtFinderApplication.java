18ee0a030f79b91f37a188a0b97ab34533f63abc
jason-ryu94 <wptmd0918@hanmail.net>
2021-11-06 02:02:56 +0900

    first commit

--- src/main/java/jason/toyproject/jmtfinder/JmtFinderApplication.java
+++ src/main/java/jason/toyproject/jmtfinder/JmtFinderApplication.java
@@ -3,6 +3,8 @@ package jason.toyproject.jmtfinder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 맛집을 저장하고 찾는 애플리케이션
+
 @SpringBootApplication
 public class JmtFinderApplication {
 
