7e6edf9dd42ae5e3b37e5493370f3250f2c38f39
jason-ryu94 <wptmd0918@hanmail.net>
2021-11-12 16:10:13 +0900

    user entity

--- src/main/java/jason/toyproject/jmtfinder/app/user/entity/User.java
+++ src/main/java/jason/toyproject/jmtfinder/app/user/entity/User.java
@@ -1,7 +1,16 @@
 package jason.toyproject.jmtfinder.app.user.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class User {
+
+    @Id
+    private String id;
+
+    private String userId;
+
+    private String userPw;
+
 }
