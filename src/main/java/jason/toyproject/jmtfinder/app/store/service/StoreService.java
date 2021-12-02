34b689ab3664f1c4aba69e7d84aad79bbfb336dd
jason-ryu94 <wptmd0918@hanmail.net>
2021-12-02 20:35:35 +0900

    store service

--- src/main/java/jason/toyproject/jmtfinder/app/store/service/StoreService.java
+++ src/main/java/jason/toyproject/jmtfinder/app/store/service/StoreService.java
@@ -1,10 +1,19 @@
 package jason.toyproject.jmtfinder.app.store.service;
 
 
+import jason.toyproject.jmtfinder.app.store.entity.StoreEntity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
+@Slf4j
+@RequiredArgsConstructor
 public class StoreService {
+
+    private StoreEntity storeEntity;
 
 
 }
