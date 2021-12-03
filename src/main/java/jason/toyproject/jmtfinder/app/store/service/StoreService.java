39e01d819a17f147698593cef7d28e388a1354af
jason-ryu94 <wptmd0918@hanmail.net>
2021-12-03 17:45:22 +0900

    retrieveStore

--- src/main/java/jason/toyproject/jmtfinder/app/store/service/StoreService.java
+++ src/main/java/jason/toyproject/jmtfinder/app/store/service/StoreService.java
@@ -2,10 +2,14 @@ package jason.toyproject.jmtfinder.app.store.service;
 
 
 import jason.toyproject.jmtfinder.app.store.entity.StoreEntity;
+import jason.toyproject.jmtfinder.app.store.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -13,7 +17,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class StoreService {
 
-    private StoreEntity storeEntity;
+    private final StoreEntity storeEntity;
+    private final StoreRepository storeRepository;
+
+    public Optional<StoreEntity> retrieveStore(String storeId) {
+        return storeRepository.findById(storeId);
+    }
 
 
 }
